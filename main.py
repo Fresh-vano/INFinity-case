774c394df7171d2e43510e0011d24f32e4f73a9f
Fresh-vano <vano94600@gmail.com>
2024-02-10 22:36:49 +0300

    Добавление кнопок и возврата назад

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 import resources
+from datetime import datetime
 
 def get_price(category, item):
     if category == 'currency':
@@ -21,12 +22,16 @@ def get_price(category, item):
 
 # Функция обработки команды старт
 def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Привет, я ТаМарКа!\nИ я хочу помочь вам с вашим вопросом.')
+    button_message(update=update, context=context)
+
+def button_message(update: Update, context: CallbackContext) -> None:
     keyboard = [
         [InlineKeyboardButton("Курсы валют", callback_data='currency')],
         [InlineKeyboardButton("Курсы металлов", callback_data='metals')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Пожалуйста, выберите категорию:', reply_markup=reply_markup)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Какой курс вы хотите узнать?", reply_markup=reply_markup)
 
 # Функция обработки кнопок
 def button(update: Update, context: CallbackContext) -> None:
@@ -34,6 +39,7 @@ def button(update: Update, context: CallbackContext) -> None:
     query.answer()
     category = query.data
     keyboard = []
+
     if category == 'currency':
         keyboard = [
             [InlineKeyboardButton("Курс юаня", callback_data='currency_yuan')],
@@ -44,6 +50,9 @@ def button(update: Update, context: CallbackContext) -> None:
             [InlineKeyboardButton("Курс стали", callback_data='metals_steel')],
             [InlineKeyboardButton("Курс чугуна", callback_data='metals_castIron')]
         ]
+
+    # Добавляем кнопку для повторного выбора
+    keyboard.append([InlineKeyboardButton("Выбрать другой курс", callback_data='restart')])
     reply_markup = InlineKeyboardMarkup(keyboard)
     query.edit_message_text(text="Выберите интересующую вас опцию:", reply_markup=reply_markup)
 
@@ -53,7 +62,28 @@ def show_price(update: Update, context: CallbackContext) -> None:
     query.answer()
     category, item = query.data.split('_')
     price = get_price(category, item)
-    query.edit_message_text(text=f"Текущая цена: {price}")
+
+    # Получаем текущую дату и время
+    current_datetime = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+
+    if price != 'Нет данных':
+        message_text = f"На момент {current_datetime} курс {item} к рублю составляет:\n1 {item} = {price} рублей"
+    else:
+        message_text = "Извините, данные временно недоступны."
+
+    # Отправляем результат в новом сообщении с кнопкой для повторного выбора
+    reply_markup = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Выбрать другой курс", callback_data='restart')]
+    ])
+    context.bot.send_message(chat_id=query.message.chat_id, text=message_text, reply_markup=reply_markup)
+
+# Функция для обработки повторного выбора
+def restart(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+
+    # Отправляем приветственное сообщение снова
+    button_message(update=update, context=context)
 
 # Основной блок
 def main():
@@ -66,7 +96,8 @@ def main():
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CallbackQueryHandler(button, pattern='^(currency|metals)$'))
     dispatcher.add_handler(CallbackQueryHandler(show_price, pattern='^(currency_|metals_)'))
-    
+    dispatcher.add_handler(CallbackQueryHandler(restart, pattern='^restart$'))
+    #dispatcher.add_handler(CallbackQueryHandler(button_message, pattern=''))
     updater.start_polling()
     updater.idle()
 
